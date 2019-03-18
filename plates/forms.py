7bf2662a2d7d5549d39e3728ad39e4c4bd1c83a6
Nicholas Utikal <nicholasutikal@gmail.com>
2019-03-18 15:34:06 +0100

    user can add new plates and display them on index view

---
+++ plates/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from .models import Plate
+
+
+class PlateForm(forms.ModelForm):
+
+    class Meta:
+        model = Plate
+        fields = ('plateNumber', 'street', 'zipCode', 'city')
