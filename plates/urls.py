5de87a09976001597af0e7d38a0a4c1155128a86
Nicholas Utikal <nutikal@ambidexter.gmbh>
2019-06-23 12:20:29 +0200

    working on redirection

--- plates/urls.py
+++ plates/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('index', views.index),
     path('new', views.new, name='plate_new'),
-    path('overview', views.overview)
+    path('', views.overview)
 ]
